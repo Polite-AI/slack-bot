a46e313c7935d3ce73650cb1238fb70bf446958f
Joseph Reeve <joseph.reeve@googlemail.com>
2017-11-18 18:16:22 +0000

    Improved config.example.js

--- config.example.js
+++ config.example.js
@@ -1,27 +1,44 @@
+const fs = require('fs');
+let botCSV = '';
+try {
+    botCSV = fs.readFileSync('./bots.csv', 'utf8');
+} catch (err) {
+    console.error(`No ./bots.csv found, please look at ./bots.example.csv`);
+}
+
+const bots = botCSV.split('\n').map(bot => {
+    const [language, personality, bot_key] = bot.split(',').map(a => a.trim());
+    return {
+        language,
+        personality,
+        bot_key
+    };
+});
+
 const config = {
     "production": {
+        "slack":{
+            "client_secret":"xxxxxxxxxxxxxxxx",
+            "client_id":"xxxxxx.xxxxxxx"
+        },
         "personalityServer": {
             "host": "localhost",
             "port": "8000",
             "version": "v1"
         },
-        "bots": [{
-            "language": "english",
-            "personality": "father-ted",
-            "bot_token": "xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        }]
+        "bots": bots
     },
     "development": {
+        "slack":{
+            "client_secret":"xxxxxxxxxxxxxxxx",
+            "client_id":"xxxxxx.xxxxxxx"
+        },
         "personalityServer": {
             "host": "localhost",
             "port": "8000",
             "version": "v1"
         },
-        "bots": [{
-            "language": "english",
-            "personality": "father-ted",
-            "bot_token": "xoxb-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        }]
+        "bots": bots
     }
 };
 
